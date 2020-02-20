b3eaf251cbfb3d57179e48e4c436e32b92759a24
Weber <anjelica.weber@mssm.edu>
2020-02-20 12:16:14 -0500

    Created the following code sections: Loading Libraries, Importing Dictionaries, Importing eIDX/IDX Data

--- V2 - MSDUS Visits Data Processing.R
+++ V2 - MSDUS Visits Data Processing.R
@@ -16,3 +16,34 @@ dictionary_eIDX_departments <- read_xlsx(path='DUS Main Dictionaries.xlsx', shee
 # Merging volID/departnent map with the cost center map
 dictionary_VolID_CC <- merge(x = dictionary_eIDX_departments, y = dictionary_Premier_volume, by.x = "VolumeID", by.y = 'Volume ID', all.x = T )
 
+
+# Importing eIDX/IDX Data -------------------------------------------------
+answer_eIDX<- select.list(choices =c('Yes','No'), title = 'Is there an eIDX/IDX file?',multiple = F, graphics = T )
+if (answer_eIDX == "Yes"){
+  path_eIDX <- choose.files(caption = "Select the eIDX/IDX file", multi = F) 
+  choices_eIDX_sheets<- c(excel_sheets(path_eIDX), "None")
+  sheet_eIDX <- select.list(choices = choices_eIDX_sheets, title = "Select eIDX Arrived Visits Sheet", graphics = T)
+  if(sheet_eIDX=="None"){
+    data_eIDX_visits <- NA
+  }else{
+    data_eIDX_visits <- as.matrix(read_xlsx(path = path_eIDX , sheet =sheet_eIDX))
+  }
+  sheet_IDX <- select.list(choices = choices_eIDX_sheets, title = 'Select IDX Arrived Visits Sheet', graphics = T) 
+  if(sheet_IDX=="None"){
+    data_IDX_visits <- NA
+  }else {
+    data_IDX_visits <- as.matrix(read_xlsx(path = path_eIDX, sheet =sheet_IDX))
+  }
+} #importing data
+if(length(data_eIDX_visits)==1 | length(data_IDX_visits==1)){
+  if (length(data_IDX_visits)==1 & length(data_eIDX_visits)!=1 ) {
+    data_eIDXIDX_visits <- data_eIDX_visits
+  }else if (length(data_IDX_visits)!=1 &  length(data_eIDX_visits)!=1){
+    data_eIDXIDX_visits<- data_IDX_visits
+  }else{
+    data_eIDXIDX_visits<- merge.data.frame(data_eIDX_visits, data_IDX_visits, all.x = T, all.y = T)
+  }
+} #merging data from eIDX/IDX
+
+
+
