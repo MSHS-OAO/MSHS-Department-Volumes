1335e1f202da548acf4d08ba5be0b7aaa0223fdd
Weber <anjelica.weber@mssm.edu>
2020-02-20 12:26:39 -0500

    Simplifying Importing Dictionaries code section, also added Epic dictionaries

--- V2 - MSDUS Visits Data Processing.R
+++ V2 - MSDUS Visits Data Processing.R
@@ -7,15 +7,22 @@ library(dplyr)
 
 # Importing Dictionaries --------------------------------------------------
 dictionary_pay_cylces <-  read_xlsx('Pay Cycles.xlsx', sheet = 1, col_types = c('date', 'skip', 'skip', 'skip', 'skip','skip', 'skip','skip', 'skip','skip','date', 'date', 'skip'))
-# Rollup + Department Map for Visits:
-dictionary_rollup_department <- read_xlsx(path = 'DUS Main Dictionaries.xlsx', sheet = 'Sch Loc to Dept Map visit e.IDX', col_types = c('skip', 'skip', 'text', 'text', 'text','skip', 'skip', 'skip'))
-# Volume ID and Cost Center Dictionary - Premier:
-dictionary_Premier_volume <- read_xlsx(path='DUS Main Dictionaries.xlsx', sheet = 'VolumeID to Cost center # Map', col_types = c('guess', 'text'), col_names = c('Volume ID', 'Cost Center'), skip = 1)
-# Volume ID (Premier),Type, and eIDX/idx Department Map:
-dictionary_eIDX_departments <- read_xlsx(path='DUS Main Dictionaries.xlsx', sheet = 'New eIDX visit - volID map', col_types = c('text', 'text', 'text', 'skip'))
-# Merging volID/departnent map with the cost center map
-dictionary_VolID_CC <- merge(x = dictionary_eIDX_departments, y = dictionary_Premier_volume, by.x = "VolumeID", by.y = 'Volume ID', all.x = T )
+dictionary_Premier_volume<- read_xlsx(path='DUS Main Dictionaries.xlsx', sheet = 'VolumeID to Cost center # Map', col_types = c('guess', 'text'), col_names = c('Volume ID', 'Cost Center'), skip = 1)
 
+#eIDX/IDX Dictionaries
+  # Rollup + Department Map for Visits:
+  dictionary_rollup_department <- read_xlsx(path = 'DUS Main Dictionaries.xlsx', sheet = 'Sch Loc to Dept Map visit e.IDX', col_types = c('skip', 'skip', 'text', 'text', 'text','skip', 'skip', 'skip'))
+  # Volume ID (Premier),Type, and eIDX/idx Department Map:
+  dictionary_eIDX_departments <- read_xlsx(path='DUS Main Dictionaries.xlsx', sheet = 'New eIDX visit - volID map', col_types = c('text', 'text', 'text', 'skip'))
+  # Merging volID/departnent map with the cost center map
+  dictionary_eIDX <- merge(x = dictionary_eIDX_departments, y = dictionary_Premier_volume, by.x = "VolumeID", by.y = 'Volume ID', all.x = T )
+
+#Epic Dictionaries
+  #Importing the Dictionaries
+  dictionary_department_VolID_Epic <- read_xlsx('DUS Main Dictionaries.xlsx', sheet = 'New Epic Volume ID Map',col_types = c('text', 'text', 'skip'))
+  #Merging Dictionaries into one
+  dictionary_EPIC <- merge(dictionary_department_VolID_Epic, dictionary_Premier_volume, by.x = 'Volume ID', by.y = 'Volume ID')
+ 
 
 # Importing eIDX/IDX Data -------------------------------------------------
 answer_eIDX<- select.list(choices =c('Yes','No'), title = 'Is there an eIDX/IDX file?',multiple = F, graphics = T )
@@ -44,6 +51,8 @@ if(length(data_eIDX_visits)==1 | length(data_IDX_visits==1)){
     data_eIDXIDX_visits<- merge.data.frame(data_eIDX_visits, data_IDX_visits, all.x = T, all.y = T)
   }
 } #merging data from eIDX/IDX
+
+# Pre Processing eIDX/IDX Data --------------------------------------------
 
 
 
