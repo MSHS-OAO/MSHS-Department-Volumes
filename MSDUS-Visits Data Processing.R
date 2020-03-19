3cfbaa9f9c2d01eb94d2135ef003a90f02eb8c1d
Weber <anjelica.weber@mssm.edu>
2020-03-19 15:12:23 -0400

    Allow user to select the Main Dicitionaries file

--- MSDUS-Visits Data Processing.R
+++ MSDUS-Visits Data Processing.R
@@ -13,19 +13,21 @@ dictionary_pay_cylces <-  read_xlsx(path_dictionary_pay_cylces, sheet = 1, col_t
   dictionary_pay_cylces$Date <- as.Date(dictionary_pay_cylces$Date)
   dictionary_pay_cylces$`Start Date` <- as.Date(dictionary_pay_cylces$`Start Date` )
   dictionary_pay_cylces$`End Date` <- as.Date(dictionary_pay_cylces$`End Date` )
-dictionary_Premier_volume<- read_xlsx(path='DUS Main Dictionaries.xlsx', sheet = 'VolumeID to Cost center # Map', col_types = c('guess', 'text'), col_names = c('Volume ID', 'Cost Center'), skip = 1)
+path_dictionary_Premier_volume <- choose.files(caption = 'Select DUS Main Dictionary',multi = F)
+dictionary_Premier_volume <- read_xlsx(path_dictionary_Premier_volume, sheet = 'VolumeID to Cost center # Map', col_types = c('guess', 'text'), col_names = c('Volume ID', 'Cost Center'), skip = 1)
+
 #eIDX/IDX Dictionaries
   # Rollup + Department Map for Visits:
-  dictionary_eIDX_rollup_department <- read_xlsx(path = 'DUS Main Dictionaries.xlsx', sheet = 'Sch Loc to Dept Map visit e.IDX', col_types = c('skip', 'skip', 'text', 'text', 'text','skip', 'skip', 'skip'))
+  dictionary_eIDX_rollup_department <- read_xlsx(path_dictionary_Premier_volume, sheet = 'Sch Loc to Dept Map visit e.IDX', col_types = c('skip', 'skip', 'text', 'text', 'text','skip', 'skip', 'skip'))
   # Volume ID (Premier),Type, and eIDX/idx Department Map:
-  dictionary_eIDX_departments <- read_xlsx(path='DUS Main Dictionaries.xlsx', sheet = 'New eIDX visit - volID map', col_types = c('text', 'text', 'text', 'skip'))
+  dictionary_eIDX_departments <- read_xlsx(path_dictionary_Premier_volume, sheet = 'New eIDX visit - volID map', col_types = c('text', 'text', 'text', 'skip'))
   # Merging volID/departnent map with the cost center map
   dictionary_eIDX <- merge(x = dictionary_eIDX_departments, y = dictionary_Premier_volume, by.x = "VolumeID", by.y = 'Volume ID', all.x = T )
   #Departments to Remove
-  remove_departments_eIDX <- read_xlsx(path='DUS Main Dictionaries.xlsx', sheet = 'eIDX_IDX Departments to Remove')
+  remove_departments_eIDX <- read_xlsx(path_dictionary_Premier_volume, sheet = 'eIDX_IDX Departments to Remove')
 #Epic Dictionaries
   #Importing the Dictionaries
-  dictionary_Epic_department_VolID <- read_xlsx('DUS Main Dictionaries.xlsx', sheet = 'New Epic Volume ID Map',col_types = c('text', 'text', 'skip'))
+  dictionary_Epic_department_VolID <- read_xlsx(path_dictionary_Premier_volume, sheet = 'New Epic Volume ID Map',col_types = c('text', 'text', 'skip'))
   #Merging Dictionaries into one
   dictionary_EPIC <- merge(dictionary_Epic_department_VolID , dictionary_Premier_volume, by.x = 'Volume ID', by.y = 'Volume ID')
   #Departments to Remove
